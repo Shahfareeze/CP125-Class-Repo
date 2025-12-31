3473c7f3854752b169f1917a62baff90d7f22085
Shahfareeze <Shahfareezeee@gmail.com>
2025-12-31 13:05:21 +0800

    update lab 3

--- labs/lab03/exercise1/exercise1.py
+++ labs/lab03/exercise1/exercise1.py
@@ -1,2 +1,15 @@
 def count_bright_spots(pixels):
+    count = 0
+    i = 0 
+    while i < len(pixels) - 1:
+        if pixels [i]== pixels[0] or pixels[i] == pixels[-1]:
+            count += 0
+        elif pixels[i] > pixels[i - 1] and pixels[i] > pixels[i+1] :
+            count += 1
+        i += 1
 
+    return count
+
+
+data = [30, 20, 10]
+print(count_bright_spots(data))
--- labs/lab03/exercise2/exercise2.py
+++ labs/lab03/exercise2/exercise2.py
@@ -1,5 +1,22 @@
 def find_station(stations, name):
-    pass
+    if name not in stations :
+        return None
+    for i in range (len(stations)):
+        if stations[i] == name:
+            return i
+        
 
 def count_stops(stations, start, stop):
-    pass
+    
+    index_start = find_station (stations, start)
+    index_stop = find_station (stations, stop)
+    if index_start == None or index_stop == None:
+        return -1
+    count = index_stop - index_start
+    count = abs(count)
+    return count
+
+
+Stations = ["Central", "Marina", "Bukit", "Orchard", "Sentosa"]
+result = find_station(Stations, "Unknown")
+print (result)
