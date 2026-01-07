1c918d1bde030ced05792e63164e4059935b55aa
Shahfareeze <Shahfareezeee@gmail.com>
2026-01-07 12:41:01 +0800

    update lab 4

--- labs/lab04/exercise4/exercise4.py
+++ labs/lab04/exercise4/exercise4.py
@@ -1,7 +1,28 @@
 
 def analyze_performance(lap_times):
-  
-    pass
+    first_laps = []
+    second_laps = []
+
+    for i in range (0,(len(lap_times))):
+        if len(lap_times)%2 == 0:
+            if i+1 <= len(lap_times) //2:
+                first_laps.append(lap_times[i])
+            else :
+                second_laps.append(lap_times[i])
+        else:
+            if i+1 <= len(lap_times)//2:
+                first_laps.append(lap_times[i])
+            else :
+                second_laps.append(lap_times[i])
+    
+    average_first = sum(first_laps)/len(first_laps)
+    average_second = sum(second_laps)/len(second_laps)
+
+    if average_second > average_first:
+        return True
+    else:
+        return False
+
 
 
 # Test
